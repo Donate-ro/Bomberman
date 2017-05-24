381c06886cc7786d59db09dba92071bb932ecd41
Vladislav <joke4789@ya.ru>
2017-05-25 00:27:19 +0300

    Added models of powerups and rotation script

---
+++ Assets/Scripts/Realize/Control/Rotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public int speedOfRotation = 50;
+    private void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * speedOfRotation, 0, Space.World);
+    }
+}
